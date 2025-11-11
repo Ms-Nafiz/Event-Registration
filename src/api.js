1b97dfc6436a3fc7458c92d0c83e1d8aaa27b81a
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-11 12:21:52 +0600

    update cors

--- src/api.js
+++ src/api.js
@@ -6,9 +6,9 @@ const API_URL = 'https://9e70b338cfb7.ngrok-free.app';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  withXSRFToken: true
-  // xsrfCookieName: 'XSRF-TOKEN',
-  // xsrfHeaderName: 'X-XSRF-TOKEN',
+  withXSRFToken: true,
+  xsrfCookieName: 'XSRF-TOKEN',
+  xsrfHeaderName: 'X-XSRF-TOKEN',
 });
 
 export default api;
