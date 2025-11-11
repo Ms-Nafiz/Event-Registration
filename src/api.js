d782c845711977e84580dbe6dcdb8064fe5fe7cf
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-11 17:54:24 +0600

    final update

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 // আপনার লারাভেল ব্যাকএন্ডের অ্যাড্রেস
-const API_URL = 'https://9e70b338cfb7.ngrok-free.app';
+const API_URL = 'https://event.cclcatv.com/';
 
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
   withXSRFToken: true,
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN',
+  // xsrfCookieName: 'XSRF-TOKEN',
+  // xsrfHeaderName: 'X-XSRF-TOKEN',
 });
 
 export default api;
