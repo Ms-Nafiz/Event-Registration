3f8774d6b1153f016cfe52aa94d4fddabfdb5279
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-12 18:39:46 +0600

    filedd

--- src/api.js
+++ src/api.js
@@ -7,17 +7,10 @@ const API_URL = "https://event.cclcatv.com";
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  // withXSRFToken: true,
+  withXSRFToken: true,
 });
 
 // api.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-// CSRF token এর জন্য request interceptor
-api.interceptors.request.use(async (config) => {
-  // প্রথমে CSRF cookie নিশ্চিত করুন
-  await axios.get('https://event.cclcatv.com/sanctum/csrf-cookie', {
-    withCredentials: true
-  });
-  return config;
-});
+
 export default api;
