d2286a96616dd57e4ca03dc59327aaef09344124
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-12 14:23:33 +0600

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 // আপনার লারাভেল ব্যাকএন্ডের অ্যাড্রেস
-const API_URL = 'https://event.cclcatv.com';
+const API_URL = "https://event.cclcatv.com";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -11,5 +12,12 @@ const api = axios.create({
 
 // api.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-
+// Interceptor — প্রতিটি request এর আগে XSRF token header এ বসানো হবে
+api.interceptors.request.use((config) => {
+  const token = Cookies.get("XSRF-TOKEN");
+  if (token) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+  return config;
+});
 export default api;
