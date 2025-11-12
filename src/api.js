c76a5f0c865c191e0f146883ce0681c65568b0e1
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-12 13:22:16 +0600

    lets try one more

--- src/api.js
+++ src/api.js
@@ -6,9 +6,10 @@ const API_URL = 'https://event.cclcatv.com';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
+  withXSRFToken: true,
 });
 
-api.defaults.xsrfCookieName = 'XSRF-TOKEN';
-api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
+// api.defaults.xsrfCookieName = 'XSRF-TOKEN';
+// api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
 
 export default api;
