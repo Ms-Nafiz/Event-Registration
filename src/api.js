a62e5aab7c288073c7380d600f2e875e1d9302c0
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-12 11:36:24 +0600

    route api check

--- src/api.js
+++ src/api.js
@@ -6,9 +6,6 @@ const API_URL = 'https://event.cclcatv.com';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  withXSRFToken: true,
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN',
 });
 
 export default api;
