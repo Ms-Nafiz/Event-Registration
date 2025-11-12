71841a059c0946466f3df791689de7feb1719bb7
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-13 00:16:02 +0600

    push

--- src/api.js
+++ src/api.js
@@ -2,15 +2,23 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // আপনার লারাভেল ব্যাকএন্ডের অ্যাড্রেস
-const API_URL = "https://event.cclcatv.com";
+const API_URL = /*"http://localhost:8000";*/"https://event.cclcatv.com";
 
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  withXSRFToken: true,
+  // withXSRFToken: true,
 });
 
-// api.defaults.xsrfCookieName = 'XSRF-TOKEN';
-// api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("XSRF-TOKEN");
+    if (token) {
+      config.headers["X-XSRF-TOKEN"] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
