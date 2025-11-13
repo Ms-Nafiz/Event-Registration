eeb9bb5cac561c965926d85348ff950510dc7e11
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-13 16:09:23 +0600

    restore

--- src/api.js
+++ src/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
-import ky from "ky";
 import Cookies from "js-cookie";
 
 // আপনার লারাভেল ব্যাকএন্ডের অ্যাড্রেস
 const API_URL = /*"http://localhost:8000";*/"https://event.cclcatv.com";
 
-const api = ky.extend({
-   prefixUrl: API_URL,
-  // withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  credentials: 'include',
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
   // withXSRFToken: true,
   // headers: {
   //   "X-Requested-With": "XMLHttpRequest",
@@ -17,15 +15,15 @@ const api = ky.extend({
 
 // api.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = Cookies.get("XSRF-TOKEN");
-//     if (token) {
-//       config.headers["X-XSRF-TOKEN"] = token;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("XSRF-TOKEN");
+    if (token) {
+      config.headers["X-XSRF-TOKEN"] = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
