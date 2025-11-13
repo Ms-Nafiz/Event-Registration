a512a65ab3b6e1d89ded1ca7c30471bc58e179b0
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-13 17:00:57 +0600

    new function added

--- src/api.js
+++ src/api.js
@@ -6,26 +6,29 @@ const API_URL = /*"http://localhost:8000";*/"https://event.cclcatv.com";
 
 const api = axios.create({
   baseURL: API_URL,
-  // withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
+  withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
   // withXSRFToken: true,
   // headers: {
   //   "X-Requested-With": "XMLHttpRequest",
   // },
 });
 
-api.defaults.withCredentials = true;
-api.defaults.withXSRFToken = true;
+// api.defaults.withCredentials = true;
+// api.defaults.withXSRFToken = true;
 // api.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-api.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get("XSRF-TOKEN");
-    if (token) {
-      config.headers["X-XSRF-TOKEN"] = token;
+// Automatically add X-XSRF-TOKEN from cookie
+axios.interceptors.request.use((config) => {
+    const cookies = document.cookie.split(';');
+    const xsrfToken = cookies
+        .find(cookie => cookie.trim().startsWith('XSRF-TOKEN='))
+        ?.split('=')[1];
+
+    if (xsrfToken) {
+        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(xsrfToken);
     }
+
     return config;
-  },
-  (error) => Promise.reject(error)
-);
+});
 
 export default api;
