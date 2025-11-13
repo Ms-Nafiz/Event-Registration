9ad966a244476925fe8b00258f182b72932c16f9
shahriar_Nafiz <msnafiz.bd@gmail.com>
2025-11-13 13:39:50 +0600

    another update

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const API_URL = /*"http://localhost:8000";*/"https://event.cclcatv.com";
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true, // কুকি আদান-প্রদানের জন্য এটি অপরিহার্য
-  withXSRFToken: true,
+  // withXSRFToken: true,
   headers: {
     "X-Requested-With": "XMLHttpRequest",
   },
@@ -15,15 +15,15 @@ const api = axios.create({
 
 // api.defaults.xsrfCookieName = 'XSRF-TOKEN';
 // api.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-api.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get("XSRF-TOKEN");
-    if (token) {
-      config.headers["X-XSRF-TOKEN"] = token;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = Cookies.get("XSRF-TOKEN");
+//     if (token) {
+//       config.headers["X-XSRF-TOKEN"] = token;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 export default api;
